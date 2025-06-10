99a2c30f103c27f8c20e702ec61a50d22437575a
JoelBlessAddo <addojoel283@gmail.com>
2025-06-10 15:57:23 +0000

    done

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -5,7 +5,7 @@ const NavbarLogo = () => {
         Joel Bless Addo
       </h1>
       <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
-        AQ
+        JAB
       </h1>
     </div>
   );
